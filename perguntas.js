faeb9466d1c451e8393e1dc55033efc681883748
erickmatheusi <00001106003949sp@al.educacao.sp.gov.br>
2024-11-05 09:39:39 -0300

    flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual seleçao mais ganhou copas do mundo?',
+    'A seleçao brasileira(Brasil)'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem tem mais bolas de ouro?',
+    'Lionel Messi'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem tem mais gols na Neo Quimica Arena?',
+    'Angel Romero'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem fez o gol na final do segundo mundial do Corinthians?',
+    'Paolo Guerrero'
+)
